5297849b804e9fe02ba70ad06eb987ed879b767c
Solimonka <xsd@yandex.ru>
2023-05-15 13:56:37 +0300

    matrix s testikami napisana 2

--- mybuild/_deps/googletest-subbuild/CMakeFiles/Makefile.cmake
+++ mybuild/_deps/googletest-subbuild/CMakeFiles/Makefile.cmake
@@ -10,10 +10,8 @@ set(CMAKE_MAKEFILE_DEPENDS
   "CMakeFiles/3.24.2/CMakeSystem.cmake"
   "CMakeLists.txt"
   "googletest-populate-prefix/tmp/googletest-populate-mkdirs.cmake"
-  "/usr/share/cmake-3.24/Modules/CMakeDetermineSystem.cmake"
   "/usr/share/cmake-3.24/Modules/CMakeGenericSystem.cmake"
   "/usr/share/cmake-3.24/Modules/CMakeInitializeConfigs.cmake"
-  "/usr/share/cmake-3.24/Modules/CMakeSystem.cmake.in"
   "/usr/share/cmake-3.24/Modules/CMakeSystemSpecificInformation.cmake"
   "/usr/share/cmake-3.24/Modules/CMakeSystemSpecificInitialize.cmake"
   "/usr/share/cmake-3.24/Modules/ExternalProject.cmake"
@@ -34,7 +32,6 @@ set(CMAKE_MAKEFILE_OUTPUTS
 
 # Byproducts of CMake generate step:
 set(CMAKE_MAKEFILE_PRODUCTS
-  "CMakeFiles/3.24.2/CMakeSystem.cmake"
   "googletest-populate-prefix/tmp/googletest-populate-mkdirs.cmake"
   "googletest-populate-prefix/tmp/googletest-populate-gitclone.cmake"
   "googletest-populate-prefix/src/googletest-populate-stamp/googletest-populate-gitinfo.txt"
